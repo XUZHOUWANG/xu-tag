234cbcebf3573577953ed21f285045a8188c30bb
xuzhouwang <741342093@qq.com>
2019-11-18 20:27:07 +0800

    init

---
+++ xutag-api/src/main/java/org/bbs4j/opensource/App.java
@@ -0,0 +1,13 @@
+package org.bbs4j.opensource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
